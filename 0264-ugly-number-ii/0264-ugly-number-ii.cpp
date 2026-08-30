78eb2256c819ba0b634286ca48560daa56701f48
nadiya-code <nadiyakola3@gmail.com>
2026-08-30 20:47:02 +0530

    Time: 3 ms (77.59%), Space: 12.4 MB (34.94%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        set<long long>st;
-        st.insert(1);
-        for(int i=0;i<n-1;i++){
-            long long x=*st.begin();
-            st.erase(st.begin());
-            st.insert(x*2);
-            st.insert(x*3);
-            st.insert(x*5);
+        vector<long long>dp(n);
+        dp[0]=1;
+        int p2=0;
+        int p3=0;
+        int p5=0;
+        for(int i=1;i<n;i++){
+            long long next2=dp[p2]*2;
+            long long next3=dp[p3]*3;
+            long long next5=dp[p5]*5;
+            dp[i]=min({next2,next3,next5});
+            if(dp[i]==next2){
+                p2++;
+            }
+            if(dp[i]==next3){
+                p3++;
+            }
+            if(dp[i]==next5){
+                p5++;
+            }
         }
-        return *st.begin();
+        return dp[n-1];
     }
 };
