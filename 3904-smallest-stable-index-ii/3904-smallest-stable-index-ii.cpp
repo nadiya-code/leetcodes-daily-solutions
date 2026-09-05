0ff1b9486389d72b93e82bac20af22459aec3485
nadiya-code <nadiyakola3@gmail.com>
2026-09-05 15:48:29 +0530

    Time: 4 ms (89.3%), Space: 202.5 MB (96.72%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -3,20 +3,14 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n=nums.size();
         vector<int>minimum(n);
-        int mini=INT_MAX;
-        int maxi=nums[0];
-        for(int i=n-1;i>=0;i--){
-            mini=min(mini,nums[i]);
-            minimum[i]=mini;
+        minimum[n-1]=nums[n-1];
+        int maxi=INT_MIN;
+        for(int i=n-2;i>=0;i--){
+            minimum[i]=min(minimum[i+1],nums[i]);
         }
-        int stableValue=maxi-minimum[0];
-        if(stableValue<=k){
-            return 0;
-        }
-        for(int i=1;i<n;i++){
+        for(int i=0;i<n;i++){
             maxi=max(maxi,nums[i]);
-            stableValue=maxi-minimum[i];
-            if(stableValue<=k){
+            if(maxi-minimum[i]<=k){
                 return i;
             }
         }
