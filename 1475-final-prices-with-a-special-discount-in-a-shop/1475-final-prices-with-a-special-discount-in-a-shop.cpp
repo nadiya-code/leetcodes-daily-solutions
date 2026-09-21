d62ac0767f93e6bbc7b6c50695ef5bb3a12a05d4
nadiya-code <nadiyakola3@gmail.com>
2026-09-21 15:16:23 +0530

    Time: 2 ms (16.92%), Space: 14.3 MB (27.8%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> finalPrices(vector<int>& prices) {
         stack<int>st;
         int m=prices.size();
-        vector<int>small(prices.begin(),prices.end());
         st.push(prices[m-1]);
         for(int i=m-2;i>=0;i--){
-            while(!st.empty()&& st.top()>prices[i]){
+            int x=prices[i];
+            while(!st.empty()&& st.top()>x){
                 st.pop();
             }
             if(!st.empty()){
-               small[i]-=st.top();
+               prices[i]-=st.top();
             }
-            st.push(prices[i]);
+            st.push(x);
         }
-        return small;
+        return prices;
     }
 };
