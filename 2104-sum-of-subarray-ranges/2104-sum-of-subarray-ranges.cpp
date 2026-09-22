41bbaa4f7b9137220a03221b891eb00afe0322af
nadiya-code <nadiyakola3@gmail.com>
2026-09-22 19:29:18 +0530

    Time: 0 ms (100%), Space: 15.8 MB (51.53%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,17 +1,67 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
+        long long summin=0;
+        long long summax=0;
         int n=nums.size();
-        long long sum=0;
+        vector<pair<int,int>>left(n);
+        stack<int>stmax;
+        stack<int>stmin;
         for(int i=0;i<n;i++){
-            int minimum=nums[i];
-            int maximum=nums[i];
-            for(int j=i+1;j<n;j++){
-                minimum=min(minimum,nums[j]);
-                maximum=max(maximum,nums[j]);
-                sum+=1LL*(maximum-minimum);
+            while(!stmin.empty() && nums[stmin.top()]>nums[i]){
+                stmin.pop();
             }
+            while(!stmax.empty() && nums[stmax.top()]<nums[i]){
+                stmax.pop();
+            }
+            if(stmin.empty()){
+                left[i].first=-1;
+            }
+            else{
+                left[i].first=stmin.top();
+            }
+            if(stmax.empty()){
+                left[i].second=-1;
+            }
+            else{
+                left[i].second=stmax.top();
+            }
+            stmin.push(i);
+            stmax.push(i);
         }
-        return sum;
+        vector<pair<int,int>>right(n);
+        while(!stmin.empty()){
+            stmin.pop();
+        }
+        while(!stmax.empty()){
+            stmax.pop();
+        }
+        for(int i=n-1;i>=0;i--){
+            while(!stmin.empty() && nums[stmin.top()]>=nums[i]){
+                stmin.pop();
+            }
+            while(!stmax.empty() && nums[stmax.top()]<=nums[i]){
+                stmax.pop();
+            }
+            if(stmin.empty()){
+                right[i].first=n;
+            }
+            else{
+                right[i].first=stmin.top();
+            }
+            if(stmax.empty()){
+                right[i].second=n;
+            }
+            else{
+                right[i].second=stmax.top();
+            }
+            stmin.push(i);
+            stmax.push(i);
+        }
+        for(int i=0;i<n;i++){
+            summin+=(1LL*nums[i]*(i-left[i].first)*(right[i].first-i));
+            summax+=(1LL*nums[i]*(i-left[i].second)*(right[i].second-i));
+        }
+        return summax-summin;
     }
 };
