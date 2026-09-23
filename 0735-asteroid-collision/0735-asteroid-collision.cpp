b1b2e3857aa0f1585bdd4be12ea423d432667551
nadiya-code <nadiyakola3@gmail.com>
2026-09-23 12:06:40 +0530

    Time: 1 ms (38.78%), Space: 22.3 MB (16.97%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int>st;
+        for(int i=0;i<asteroids.size();i++){
+            if(st.empty()){
+                st.push(asteroids[i]);
+            }
+            else if(!st.empty() && st.top()>0 && asteroids[i]>0){
+                st.push(asteroids[i]);
+            }
+            else if(!st.empty() && st.top()>0 && asteroids[i]<0){
+                if(st.top()<abs(asteroids[i])){
+                    while(!st.empty() && st.top()>0 && st.top()<abs(asteroids[i])){
+                        st.pop();
+                    }
+                    if(!st.empty() && st.top()==abs(asteroids[i])){
+                        st.pop();
+                    }
+                    else if(st.empty()||(!st.empty()&& st.top()<0)){
+                        st.push(asteroids[i]);
+                    }
+                }
+                else if(st.top()==abs(asteroids[i])){
+                    st.pop();
+                }
+            }
+            else if(!st.empty() && st.top()<0 && asteroids[i]>0){
+                st.push(asteroids[i]);
+            }
+            else if(!st.empty() && st.top()<0 && asteroids[i]<0){
+                st.push(asteroids[i]);
+            }
+        }
+        int size=st.size();
+        vector<int>ans(size);
+        size--;
+        while(!st.empty()){
+            ans[size--]=st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
