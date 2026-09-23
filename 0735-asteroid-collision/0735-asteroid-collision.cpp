11d4176567eea04612b4a236e23b85d65abd702d
nadiya-code <nadiyakola3@gmail.com>
2026-09-23 13:48:03 +0530

    Time: 3 ms (28.08%), Space: 22.3 MB (6.07%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -3,10 +3,7 @@ public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int>st;
         for(int i=0;i<asteroids.size();i++){
-            if(!st.empty() && st.top()>0 && asteroids[i]>0){
-                st.push(asteroids[i]);
-            }
-            else if(!st.empty() && st.top()>0 && asteroids[i]<0){
+            if(!st.empty() && st.top()>0 && asteroids[i]<0){
                 if(st.top()<abs(asteroids[i])){
                     while(!st.empty() && st.top()>0 && st.top()<abs(asteroids[i])){
                         st.pop();
