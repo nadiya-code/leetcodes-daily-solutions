c5b6c962e1e48ec3988615ad4e79abcd9ba36183
nadiya-code <nadiyakola3@gmail.com>
2026-09-25 13:01:56 +0530

    Time: 0 ms (100%), Space: 23.6 MB (92.89%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,17 +3,45 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int>array;
         int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i+n/3<n;i++){
-            if(nums[i+n/3]==nums[i]){
-                array.push_back(nums[i]);
-                int prev=nums[i];
-                i+=n/3;
-                while(i<n && nums[i]==prev){
-                    i++;
-                }
-                i--;
+        int c1=0;
+        int c2=0;
+        int el1=0;
+        int el2=0;
+        for(int & num :nums){
+            if(el1==num){
+                c1++;
             }
+            else if(el2==num){
+                c2++;
+            }
+            else if(c1==0){
+                el1=num;
+                c1++;
+            }
+            else if(c2==0){
+                el2=num;
+                c2++;
+            }
+            else{
+                c1--;
+                c2--;
+            }
+        }
+        c1=0;
+        c2=0;
+        for(int num :nums){
+            if(num==el1){
+                c1++;
+            }
+            else if(num==el2){
+                c2++;
+            }
+        }
+        if(c1>n/3){
+            array.push_back(el1);
+        }
+        if(c2>n/3){
+            array.push_back(el2);
         }
         return array;
     }
